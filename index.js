b430de6a1fe253c041ce63be9d896ba7d7fb149c
waleed <waleedmail@gmail.com>
2024-04-27 19:32:37 +0300

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("node:http");
+
+//console.log(http);
+
+const server = http.createServer((req, res)=>{
+    const str = req.url;
+    let arr = str.match(/\d+/g);
+    let a = +arr[0];
+    let b = +arr[1];
+    let result = 0;
+    if(str.match(/add/)){
+        result = a + b;
+    }
+    else if(str.match(/subtract/)){
+        result = a - b;
+    }
+    else if(str.match(/multiply/)){
+        result = a * b;
+    }
+    else{
+        result = a / b;
+    }
+    res.end(`Response => ${result}`);
+});
+
+server.listen(3000, ()=>{
+    console.log("listen to 3000");
+});
